8113e6c2c3e3356714a08944cb5f7dfc139f0e5f
DonStylz74 <84778838+DonStylz74@users.noreply.github.com>
2024-10-26 16:54:41 +1100

    Update config.lua
    extra shops

--- config.lua
+++ config.lua
@@ -20,7 +20,7 @@ Config.Setup = {
     -- Target system, available options are: 'ox_target', 'qb-target', 'qtarget', 'custom' & 'none'
     -- 'custom' needs to be added to client/functions.lua
     -- If 'none' then TextUI is used instead of targeting
-    target = 'none',
+    target = 'ox_target',
     -- Notification system, available options are: 'ox_lib', 'esx', 'qb', 'okok' & 'custom'
     -- 'custom' needs to be added to client/functions.lua
     notify = 'ox_lib',
@@ -45,25 +45,24 @@ Config.Shops = {
         name = 'Vinewood Pawn & Jewelry', -- Shop name
         slots = 25, -- How many slots are available
         weight = 100000, -- How much weight is available
-        coords = vec4(-1459.2361, -413.2576, 36.2567, 343.8426), -- Where this shop exists
+        coords = vec4(-474.2806, 278.9507, 83.2940, 148.4663), -- Where this shop exists
         radius = 1.0, -- How large of a circle zone radius (for targeting only)
-        spawnPed = false, -- Spawn a ped to interact with here?
-        pedModel = 'a_m_y_beach_02', -- If spawnPed = true, what ped model?
+        spawnPed = true, -- Spawn a ped to interact with here?
+        pedModel = 'A_M_Y_Business_02', -- If spawnPed = true, what ped model?
         -- You can limit the hours at which the shop is available here
         -- Min is the earliest the shop is available (default 06:00AM)
         -- Max is the latest the shop is available (detault 21:00 aka 9PM)
-        -- If you want it available 24/7, set min to 0 and max to 24
+        -- If you want it available 24/7, set min to 1 and max to 24
         hour = { min = 6, max = 21 },
         account = 'cash', -- Give 'cash', 'bank' or 'dirty' money when selling here?
         allowlist = {
             -- What items can be sold here
             -- Any item not in this list, cannot be sold here
             -- ['itemSpawnName'] = { label = 'Item Name', price = sellPrice }
-            ['water'] = { label = 'Water', price = 50 },
-            ['panties'] = { label = 'Knickers', price = 10 },
+            ['water'] = { label = 'Water', price = 5 },
+            ['burger'] = { label = 'Water', price = 5 },
             ['lockpick'] = { label = 'Lockpick', price = 25 },
-            ['phone'] = { label = 'Phone', price = 150 },
-            ['armour'] = { label = 'Bulletproof Vest', price = 225 },
+            ['hotwire'] = { label = 'Cutters', price = 150 },
             -- Add & remove items here as desired
             -- Be sure to follow the same format as above
         },
@@ -76,33 +75,82 @@ Config.Shops = {
             enabled = true, -- Enable or disable the blip for this shop
             sprite = 59, -- Sprite ID (https://docs.fivem.net/docs/game-references/blips/)
             color = 0, -- Color (https://docs.fivem.net/docs/game-references/blips/#blip-colors)
-            scale = 0.8, -- Size/scale
+            scale = 0.5, -- Size/scale
             label = 'Pawn Shop' -- Label
         }
     },
-    ['strawberry'] = {
-        name = 'Strawberry Ave Pawn Shop',
+    ['davis'] = {
+        name = 'Davis Pawn Shop',
         slots = 25,
         weight = 100000,
-        coords = vec4(182.7942, -1319.3451, 29.3173, 244.3924),
+        coords = vec4(130.1092, -1775.4546, 29.7262, 291.5801),
         radius = 1.0,
         spawnPed = true,
-        pedModel = 'a_m_y_beach_02',
+        pedModel = 'A_M_Y_Business_02',
         hour = { min = 6, max = 21 },
         account = 'cash',
         allowlist = {
-            ['water'] = { label = 'Water', price = 50 },
-            ['panties'] = { label = 'Knickers', price = 10 },
+            ['water'] = { label = 'Water', price = 5 },
+            ['burger'] = { label = 'Water', price = 5 },
             ['lockpick'] = { label = 'Lockpick', price = 25 },
-            ['phone'] = { label = 'Phone', price = 150 },
-            ['armour'] = { label = 'Bulletproof Vest', price = 225 },
+            ['hotwire'] = { label = 'Cutters', price = 150 },
         },
-        placeholders = false,
+        placeholders = true,
         blip = {
             enabled = true,
             sprite = 59,
             color = 0,
-            scale = 0.8,
+            scale = 0.5,
+            label = 'Pawn Shop'
+        }
+    },
+    ['sandyshores'] = {
+        name = 'Sandy Shores Pawn Shop',
+        slots = 25,
+        weight = 100000,
+        coords = vec4(1708.0963, 3785.9739, 34.7110, 195.6421),
+        radius = 1.0,
+        spawnPed = true,
+        pedModel = 'A_M_Y_Business_02',
+        hour = { min = 6, max = 21 },
+        account = 'cash',
+        allowlist = {
+            ['water'] = { label = 'Water', price = 5 },
+            ['burger'] = { label = 'Water', price = 5 },
+            ['lockpick'] = { label = 'Lockpick', price = 25 },
+            ['hotwire'] = { label = 'Cutters', price = 150 },
+        },
+        placeholders = true,
+        blip = {
+            enabled = true,
+            sprite = 59,
+            color = 0,
+            scale = 0.5,
+            label = 'Pawn Shop'
+        }
+    },
+    ['paletobay'] = {
+        name = 'Paleto Bay Pawn Shop',
+        slots = 25,
+        weight = 100000,
+        coords = vec4(-310.3446, 6185.5430, 31.5605, 35.7470),
+        radius = 1.0,
+        spawnPed = true,
+        pedModel = 'A_M_Y_Business_02',
+        hour = { min = 6, max = 21 },
+        account = 'cash',
+        allowlist = {
+            ['water'] = { label = 'Water', price = 5 },
+            ['burger'] = { label = 'Water', price = 5 },
+            ['lockpick'] = { label = 'Lockpick', price = 25 },
+            ['hotwire'] = { label = 'Cutters', price = 150 },
+        },
+        placeholders = true,
+        blip = {
+            enabled = true,
+            sprite = 59,
+            color = 0,
+            scale = 0.5,
             label = 'Pawn Shop'
         }
     },
